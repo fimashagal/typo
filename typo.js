959cbd1a393a88276b3f75a4ead905598bb9e5c5
Trofim Shagal <alt.fimashagal@gmail.com>
2018-07-11 01:08:12 +0300

    Update typo.js

--- typo.js
+++ typo.js
@@ -17,7 +17,7 @@
     };
 
     Typo.prototype.isDef = function (object = null) {
-        return !/null|undefined/.test(this.typeOf(object)) && !isNaN(object);
+        return !/null|undefined/.test(this.typeOf(object)) || (this.typeOf(object) === "number" && !isNaN(object));
     };
 
     Typo.prototype.isFn = function (object = null) {
