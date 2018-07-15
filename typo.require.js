e504ac1cf1c0344e0344179312c943d169e56585
Trofim Shagal <alt.fimashagal@gmail.com>
2018-07-15 17:21:21 +0300

    Update typo.require.js

--- typo.require.js
+++ typo.require.js
@@ -4,9 +4,9 @@ function Typo() {}
 
 Typo.prototype.typeOf = function (object = null) {
     return Object.prototype.toString
-            .call(object)
-            .replace(/^\[object (.+)\]$/, '$1')
-            .toLowerCase();
+        .call(object)
+        .replace(/^\[object (.+)\]$/, '$1')
+        .toLowerCase();
 };
 
 Typo.prototype.typify = function (object = null) {
@@ -48,12 +48,20 @@ Typo.prototype.isElement = function(object = null){
 Typo.prototype.isEmpty = function (object = null) {
     let type = this.typeOf(object),
         response = false;
-    if(((type === "string" && object === "")
-        || (/array|htmlcollection|nodelist/.test(type) && !object.length)
-        || (/set|map/.test(type) && !object.size)
-        || (type === "object" && !Object.keys(object).length)
-        || (this.isElement(object) && (!object.children.length && !object.childNodes.length)))
-        && !this.isDef(object)){
+    if(!this.isDef(object)) return response;
+    if(type === "string" && object === ""){
+        response = true;
+    }
+    if(/array|htmlcollection|nodelist/.test(type) && object.length === 0){
+        response = true;
+    }
+    if(/set|map/.test(type) && !object.size){
+        response = true;
+    }
+    if(type === "object" && !Object.keys(object).length){
+        response = true;
+    }
+    if(this.isElement(object) && (!object.children.length && !object.childNodes.length)){
         response = true;
     }
     return response;
@@ -71,8 +79,8 @@ Typo.prototype.isTouch = function(ctx = null){
     if(this.isDef(window)) ctx = window;
     if(!this.isDef(ctx)) return false;
     return 'ontouchstart' in ctx
-            || navigator.MaxTouchPoints > 0
-            || navigator.msMaxTouchPoints > 0;
+        || navigator.MaxTouchPoints > 0
+        || navigator.msMaxTouchPoints > 0;
 };
 
 define(function () {
