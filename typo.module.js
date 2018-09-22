ac44d2d8f603566bcd8fb1f11a07dfc9fc583940
fimashagal <fimashagal@gmail.com>
2018-09-22 14:21:33 +0300

    append new method

--- typo.module.js
+++ typo.module.js
@@ -17,122 +17,105 @@ Typo.prototype.typify = function (object = null) {
 };
 
 Typo.prototype.isDef = function (object = null, fnTrue = null, fnFalse = null, fnAfter = null) {
-    let response = !/null|undefined/.test(this.typeOf(object)) || (this.typeOf(object) === "number" && !isNaN(object));
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = !/null|undefined/.test(this.typeOf(object)) || (this.typeOf(object) === "number" && !isNaN(object));
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isntDef = function(object = null, fnTrue = null, fnFalse = null, fnAfter = null){
-    let response = !this.isDef(object);
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = !this.isDef(object);
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isFn = function (object = null, fnTrue = null, fnFalse = null, fnAfter = null) {
-    let response = /function/.test(this.typeOf(object));
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = /function/.test(this.typeOf(object));
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isString = function(object = null, fnTrue = null, fnFalse = null, fnAfter = null){
-    let response = typeof object === "string" || object instanceof String;
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = typeof object === "string" || object instanceof String;
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isNumber = function(object = null, fnTrue = null, fnFalse = null, fnAfter = null){
-    let response = this.typeOf(object) === "number" && !isNaN(object) && isFinite(object);
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = this.typeOf(object) === "number" && !isNaN(object) && isFinite(object);
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isFloat = function(object = null, fnTrue = null, fnFalse = null, fnAfter = null){
-    let response = this.isNumber(object) && /[.]/.test(`${object}`);
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = this.isNumber(object) && /[.]/.test(`${object}`);
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isInteger = function(object = null, fnTrue = null, fnFalse = null, fnAfter = null){
-    let response = this.isNumber(object) && !/[.]/.test(`${object}`);
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = this.isNumber(object) && !/[.]/.test(`${object}`);
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isHEX = function(object = null, fnTrue = null, fnFalse = null, fnAfter = null){
     object = object.replace(/[#]|[0x]/g, '');
-    let response = parseInt(object, 16).toString(16) === object;
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = parseInt(object, 16).toString(16) === object;
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isElement = function(object = null, fnTrue = null, fnFalse = null, fnAfter = null){
-    let response;
+    let condition;
     try {
         if(typeof object === "string") object = document.querySelector(object);
-        response = /^(html)+(.)+(element)$|htmlelement/gm.test(this.typeOf(object));
-        this._eventually(response, fnTrue, fnFalse, fnAfter);
-        return response;
+        condition = /^(html)+(.)+(element)$|htmlelement/gm.test(this.typeOf(object));
+        return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
     } catch(err){
-        console.warn(err);
-        response = false;
-        this._eventually(response, fnTrue, fnFalse, fnAfter);
-        return response;
+        condition = false;
+        return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
     }
 };
 
 Typo.prototype.isEmpty = function (object = null, fnTrue = null, fnFalse = null, fnAfter = null) {
     let type = this.typeOf(object),
-        response = false;
+        condition = false;
     if(!this.isDef(object)) return true;
     if(type === "string" && object === ""){
-        response = true;
+        condition = true;
     }
     if(/array|htmlcollection|nodelist/.test(type) && object.length === 0){
-        response = true;
+        condition = true;
     }
     if(/set|map/.test(type) && !object.size){
-        response = true;
+        condition = true;
     }
     if(type === "object" && !Object.keys(object).length){
-        response = true;
+        condition = true;
     }
     if(this.isElement(object) && (!object.children.length && !object.childNodes.length)){
-        response = true;
+        condition = true;
     }
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isntEmpty = function (object = null, fnTrue = null, fnFalse = null, fnAfter = null) {
-    let response = !this.isEmpty(object);
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = !this.isEmpty(object);
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isChar = function (object = null, fnTrue = null, fnFalse = null, fnAfter = null) {
-    let response = this.typeOf(object) === "string" && object.length === 1;
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = this.typeOf(object) === "string" && object.length === 1;
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isURL = function (object = null, fnTrue = null, fnFalse = null, fnAfter = null) {
-    let response = this.typeOf(object) === "string" && /(https?:\/\/[^\s]+)/g.test(object);
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = this.typeOf(object) === "string" && /(https?:\/\/[^\s]+)/g.test(object);
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isURI = function (object = null, fnTrue = null, fnFalse = null, fnAfter = null) {
-    let response = this.isURL(object) && /([.]+(avi|mp4|ogg|wav|mp3|svg|jpg|jpeg|png|gif|webm|webp|json)$)/.test(object);
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = this.isURL(object) && /([.]+(avi|mp4|ogg|wav|mp3|svg|jpg|jpeg|png|gif|webm|webp|json)$)/.test(object);
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isTouch = function(ctx = null, fnTrue = null, fnFalse = null, fnAfter = null){
     if(this.isDef(window)) ctx = window;
     if(this.isntDef(ctx)) return false;
-    let response = 'ontouchstart' in ctx || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    let condition = 'ontouchstart' in ctx || navigator.MaxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isFacebookBrowser = function(ctx = null, fnTrue = null, fnFalse = null, fnAfter = null){
@@ -141,13 +124,12 @@ Typo.prototype.isFacebookBrowser = function(ctx = null, fnTrue = null, fnFalse =
     let userAgent = navigator.userAgent
         || navigator.vendor
         || window.opera,
-        response = /FBAN|FBAV/.test(userAgent);
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+        condition = /FBAN|FBAV/.test(userAgent);
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
 };
 
 Typo.prototype.isTypeChain = function(collection = [], typeChain = [], fnTrue = null, fnFalse = null, fnAfter = null){
-    let response = true;
+    let condition = true;
     let typeOfCollection = this.typeOf(collection);
 
     if(typeOfCollection === "object") collection = Object.values(collection);
@@ -155,19 +137,24 @@ Typo.prototype.isTypeChain = function(collection = [], typeChain = [], fnTrue = 
     if(typeOfCollection === "set") collection = [...collection];
 
     if(collection.length !== typeChain.length || /number|string|boolean|null|undefined/.test(typeOfCollection)) {
-        response = false;
+        condition = false;
     } else {
         for(let i = 0; i < typeChain.length; i++){
             let chain = typeChain[i];
             (chain === "undefined") && (chain = "null");
             if(this.typeOf(collection[i]) !== chain){
-                response = false;
+                condition = false;
                 break;
             }
         }
     }
-    this._eventually(response, fnTrue, fnFalse, fnAfter);
-    return response;
+    return this._pipe({ condition, fnTrue, fnFalse, fnAfter });
+};
+
+Typo.prototype._pipe = function (options = {}) {
+    const {condition, fnTrue, fnFalse, fnAfter } = options;
+    this._eventually(condition, fnTrue, fnFalse, fnAfter);
+    return condition;
 };
 
 Typo.prototype._eventually = function (condition, fnA, fnB, fnC) {
